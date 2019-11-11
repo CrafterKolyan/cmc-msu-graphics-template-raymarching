9a8ffba09603e7c9f26611c63ad23fdffce48f00
Nikolay Korolev <CrafterKolyan@mail.ru>
2019-11-11 13:24:47 +0300

    Fix copy paste error

--- LiteMath.h
+++ LiteMath.h
@@ -28,7 +28,7 @@ namespace LiteMath {
 
         float3(float a, float b, float c) : x(a), y(b), z(c) {}
 
-        float3(const float *ptr) : x(ptr[0]), y(ptr[1]), z(ptr[0]) {}
+        float3(const float *ptr) : x(ptr[0]), y(ptr[1]), z(ptr[2]) {}
 
         float x, y, z;
     };
@@ -46,7 +46,7 @@ namespace LiteMath {
 
         int3(int a, int b, int c) : x(a), y(b), z(c) {}
 
-        int3(const int *ptr) : x(ptr[0]), y(ptr[1]), z(ptr[0]) {}
+        int3(const int *ptr) : x(ptr[0]), y(ptr[1]), z(ptr[2]) {}
 
         int x, y, z;
     };
